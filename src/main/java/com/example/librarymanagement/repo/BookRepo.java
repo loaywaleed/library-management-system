bc22dc79c412c4e9c8a9ff117a9f8bfbdf9d7791
Loay Waleed <loaywaleed9@gmail.com>
2024-12-04 15:59:30 +0200

    Book Repo

--- src/main/java/com/example/librarymanagement/repo/BookRepo.java
+++ src/main/java/com/example/librarymanagement/repo/BookRepo.java
@@ -1,4 +1,10 @@
 package com.example.librarymanagement.repo;
+import com.example.librarymanagement.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-public class BookRepo {
+@Repository
+public interface BookRepo extends JpaRepository<Book, Long> {
+    Optional<Book> findByIsbn(String isbn);
 }
