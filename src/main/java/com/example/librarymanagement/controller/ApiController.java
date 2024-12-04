4d8965fd08d8f3c872f156726677f4296fe4b756
Loay Waleed <loaywaleed9@gmail.com>
2024-12-04 15:25:48 +0200

    Project setup

---
+++ src/main/java/com/example/librarymanagement/controller/ApiController.java
@@ -0,0 +1,7 @@
+package com.example.librarymanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class ApiController {
+
+}
---
+++ src/main/java/com/example/librarymanagement/repo/BookRepo.java
@@ -0,0 +1,4 @@
+package com.example.librarymanagement.repo;
+
+public class BookRepo {
+}
