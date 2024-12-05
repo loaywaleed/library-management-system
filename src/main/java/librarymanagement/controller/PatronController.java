03530e4b76ec602a7b962b02ede7c9a44d7d859f
Loay Waleed <loaywaleed9@gmail.com>
2024-12-05 19:27:08 +0200

    Updated patron controller

--- src/main/java/librarymanagement/controller/PatronController.java
+++ src/main/java/librarymanagement/controller/PatronController.java
@@ -4,13 +4,13 @@ package librarymanagement.controller;
 //import com.library.rest.models.Patron;
 //import com.library.rest.repo.PatronRepo;
 import librarymanagement.exception.ResourceNotFoundException;
-import librarymanagement.exception.ConflictException;
 import librarymanagement.model.Patron;
 import librarymanagement.repository.PatronRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import librarymanagement.service.PatronService;
 
 import java.util.List;
 
@@ -20,46 +20,34 @@ import java.util.List;
 public class PatronController {
 
     @Autowired
-    private PatronRepository patronRepo;
+    private PatronService patronService;
 
 
     @GetMapping(value = "/patrons")
     public List<Patron> getPatron() {
-        return patronRepo.findAll();
+        return patronService.getAllPatrons();
     }
 
     // Create a new patron
     @PostMapping(value = "/patrons")
     public ResponseEntity<Patron> createPatron(@RequestBody Patron patron) {
-        if (patronRepo.findByName(patron.getName()).isPresent()) {
-            throw new ConflictException("A patron with the name " + patron.getName() + " already exists.");
-        }
-        Patron savedPatron = patronRepo.save(patron);
+        Patron savedPatron = patronService.createPatron(patron);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedPatron);
     }
 
-
     @GetMapping(value ="/patrons/{id}")
     public Patron getPatronById(@PathVariable long id) {
-        return patronRepo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Book", "id", id));
+        return patronService.getPatronById(id);
     }
 
     @DeleteMapping(value ="/patrons/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePatronById(@PathVariable long id) {
-        Patron book = patronRepo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Book", "id", id));
-        patronRepo.delete(book);
+        patronService.deletePatronById(id);
     }
 
     @PutMapping(value = "/patrons/{id}")
     public Patron updateBookById(@PathVariable long id, @RequestBody Patron patron) {
-        Patron patronToUpdate = patronRepo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Book", "id", id));
-        patronToUpdate.setName(patron.getName());
-        patronToUpdate.setEmail(patron.getEmail());
-        patronToUpdate.setPhone(patron.getPhone());
-        return patronRepo.save(patronToUpdate);
+        return patronService.updatePatronById(id, patron);
     }
 }
